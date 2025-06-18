418eda3137cd94f020b731f5968568df512e266b
dimash <dimash@coveofedu.org>
2025-06-19 02:19:35 +0500

    fix broken react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import routes from 'virtual:generated-pages-react';
 import Layout from './components/layout';
 import './index.css';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   routes.map((route) => ({
     ...route,
     element: <Layout>{route.element}</Layout>,
